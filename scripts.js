c20d505ddf10603e2cba6849fc52b525716ec23a
Sploob <crayiphone@gmail.com>
2022-02-01 19:47:40 -0800

    update boldPassage function to reove erroneous space at the end of the returned string Co-Authored by: Hans Schroeder <hajschroeder@gmail.com>

--- scripts.js
+++ scripts.js
@@ -43,9 +43,18 @@ $(document).ready(function(){
 });
 
 function boldPassage(word, text) {
-  if (word === text) {
-    return "<p><b>" + text + "</b></p>";
-  } else
-  return "<p>" + text + "</p>";
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (word === element) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1 )) {
+    htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
 }
 
