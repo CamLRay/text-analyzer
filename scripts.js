c10911d6129594b4dbb817f0f64267749ff71a6f
Sploob <crayiphone@gmail.com>
2022-02-02 20:38:09 -0800

    BUGFIX: NumberofOccurrencesInText function to not count words containing a string Co-Authored by: Hans Schroeder <hajschroeder@gmail.com>

--- scripts.js
+++ scripts.js
@@ -25,7 +25,7 @@ function numberOfOccurrencesInText(word, text) {
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
+    if (element.toLowerCase() === (word.toLowerCase())) {
       wordCount++
     }
   });
